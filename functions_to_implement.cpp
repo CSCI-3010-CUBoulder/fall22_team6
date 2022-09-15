067b7a2d7cdff1b33cc9fad9baa9e66a4302e713
bnies1017 <ben.nies1017@gmail.com>
2022-09-15 11:35:54 -0600

    initial commit

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -1,4 +1,5 @@
 /* String functions section */
+#include <iostream>
 
 // Splits a single string on separator into a vector of strings
 std::vector<std::string> Split(std::string whole, std::string separator);
